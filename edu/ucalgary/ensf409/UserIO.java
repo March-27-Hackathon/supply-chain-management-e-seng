130966320f963affde72df946be67caa9deef7e5
SovietCheburek <77901186+SovietCheburek@users.noreply.github.com>
2021-03-27 21:58:21 -0600

    Update UserIO.java

--- edu/ucalgary/ensf409/UserIO.java
+++ edu/ucalgary/ensf409/UserIO.java
@@ -28,37 +28,40 @@ package edu.ucalgary.ensf409;
 		 String code="";
 		 String quantity="";
 		 while(hold==-1){
-		 System.out.println("Please enter your desired furniture type:");
+		 System.out.println("Please enter your desired furniture type or type 'Q' to quit:");
 		 this.request = System.console().readLine();
+		 if(this.request.equalsIgnoreCase("Q")){
+			 System.exit(1);
+		 }
 		 System.out.println("Please enter quantity:");
 		quantity=System.console().readLine();
 		 this.request=this.request.trim();
 		String [] items=this.request.split(" ");
 		 quantity=quantity.trim();
-		 code+=items[0].charAt(0);
-		 code+=items[1].charAt(0);
-		 if(items[0]==null||items[1]==null){
-			 continue;
-		 }
 		 try{
 		 quan = Integer.parseInt(quantity);
 		 if(quan<=0){
 			 throw new IllegalArgumentException();
 		 }
-		 if(items.length<2){
+		 if(items.length>2){
 			 throw new Exception();
+		 }
+		 if(items[0]==null||items[1]==null){
+			 continue;
 		 }
 		 }catch(IllegalArgumentException ex){
 			 System.out.println("Invalid quantity");
 			 continue;
 		 }catch(Exception e){
-			 System.out.println("Invalid furniture type");
+			 System.out.println("Invalid furniture type that");
 			 continue;
 		 }
+		 code+=items[0].charAt(0);
+		 code+=items[1].charAt(0);
 		 code+=quantity;
 		 hold=manage.parseOrder(items[0].toLowerCase(),items[1].toLowerCase(),quan);
 		 if(hold==-1){
-			 System.out.println("Invalid furniture type");
+			 System.out.println("Invalid furniture type this");
 		 }else if(hold==-2){
 			 System.out.println("Order cannot be fulfilled based on current inventory");
 			 String[] manufaclist= this.manage.getManufacturersList(items[0].toLowerCase(),items[1].toLowerCase());
